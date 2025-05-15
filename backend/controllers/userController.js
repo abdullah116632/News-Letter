971335833bef1e5af7d54418f1a1e5d8c17d6416
Abdullah al rafi <abdullah116632@gmail.com>
2025-05-15 22:34:35 +0600

    get user by email

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,13 +2,36 @@ import User from "../models/userModel.js";
 import CustomError from "../utils/customErrorClass.js";
 
 
-
 export const getMe = async (req, res, next) => {
   try {
     // req.user is set by protect middleware
     const userId = req.user.id;
 
     const user = await User.findById(userId).select("-password -confirmPassword");
+    if (!user) {
+      return next(new CustomError(404, "User not found"));
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getUserByEmail = async (req, res, next) => {
+  try {
+    const { email } = req.params;
+
+    if (!email) {
+      return next(new CustomError(400, "Email parameter is required"));
+    }
+
+    const user = await User.findOne({ email }).select("-password -confirmPassword");
     if (!user) {
       return next(new CustomError(404, "User not found"));
     }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { getMe } from "../controllers/userController.js";
+import { getMe, getUserByEmail } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getMe)
+router.get("/:email", protectRoute, getUserByEmail)
 
 export default router;
